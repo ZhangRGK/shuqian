ee077827044fcb6178e15ece35123c7ca82f718b
张锐 <stamen0630@gmail.com>
2014-06-27 17:10:20 +0800

    删除注释与无用的方法

--- public/chrome/bigmarks.js
+++ public/chrome/bigmarks.js
@@ -1,7 +1,6 @@
 var userId = localStorage.getItem('userId');
 var userEmail = localStorage.getItem("userEmail");
 var serviceUrl = 'http://shuqian.bigzhu.org';
-//var serviceUrl = 'http://localhost:3000';
 
 var init = function() {
     if (userId === null || userId === "null") {
@@ -28,29 +27,13 @@ $("#openApp").on("click", function () {
     chrome.tabs.create({"url": serviceUrl});
 });
 
-function post(url, data){
-    var method = "POST";
-
-    var async = true;
-
-    var request = new XMLHttpRequest();
-
-    request.open(method, url, async);
-
-    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-    //request.onreadystatechange(function(http) {
-    //    if(http.readyState == 4 && http.status == 200) {
-    //        chrome.browserAction.setBadgeText({"text": ""});
-    //    }
-    //});
-
-    request.send(data);
-}
 $("#uploadToDefault").on("click", function() {
     chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
         chrome.browserAction.setBadgeText({"text": "↑↑"});
-        post(serviceUrl+"/upload",JSON.stringify({"userId":null,"data":bookmarkTreeNodes}));
+        $.post(serviceUrl+"/upload",{"userId":null,"data":bookmarkTreeNodes}, function(data,status) {
+            console.log("data:"+data+" status:"+status);
+            chrome.browserAction.setBadgeText({"text": ""});
+        });
     });
 });
 
@@ -64,32 +47,20 @@ $("#uploadToUser").on("click", function() {
     });
 });
 
-/*
-function remove(id, bookmarks, test){
-    console.log("remove");
-    console.log(bookmarks);
-    console.log(test);
-    node2 = chrome.bookmarks.get(id, function(node){
-            console.log(node);
-            post(serviceUrl+"/remove", JSON.stringify(bookmarks));
-            });
-    console.log(node2);
-}
-*/
-
 function update(id, bookmarks){
     console.log("update");
     chrome.browserAction.setBadgeText({"text":"↑"});
     console.log(bookmarks);
     bookmarks.id = id;
-    post(serviceUrl+"/update", JSON.stringify({"userId":userId,"data":bookmarks}));
+    $.post(serviceUrl+"/update",{"userId":userId,"data":bookmarks},function(data,status) {
+        console.log("data:"+data+" status:"+status);
+        chrome.browserAction.setBadgeText({"text": ""});
+    });
 }
-//chrome.bookmarks.onRemoved.addListener(remove);
+
 chrome.bookmarks.onCreated.addListener(function(id,bookmark) {
     console.log(id,":",bookmark);
 });
-//只有title和url改变时会触发
-//chrome.bookmarks.onChanged.addListener(update);
 
 console.log(chrome.bookmarks.onMoved);
 chrome.bookmarks.onMoved.addListener(update);
